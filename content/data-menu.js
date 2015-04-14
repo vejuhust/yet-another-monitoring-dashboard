3f794f4ee52529f8d36aa743ad4a24aa776c862b
Wei Ye <vejuhust@gmail.com>
2015-04-14 18:48:51 +0800

    Add content/data-menu.js to make menu items configurable

---
+++ content/data-menu.js
@@ -0,0 +1,63 @@
+var menu_data = [{
+    "name": "Global",
+    "icon": "fa fa-globe",
+    "link": "#global",
+}, {
+    "name": "Region 1",
+    "icon": "flag-icon flag-icon-us",
+    "sub": [{
+        "name": "Summary",
+        "link": "#reg-1",
+    }, {
+        "name": "Environment 1-1",
+        "link": "#env-1-1",
+    }, {
+        "name": "Environment 1-2",
+        "link": "#env-1-2",
+    }, {
+        "name": "Environment 1-3",
+        "link": "#env-1-3",
+    }],
+}, {
+    "name": "Region 2",
+    "icon": "flag-icon flag-icon-hk",
+    "sub": [{
+        "name": "Summary",
+        "link": "#reg-2",
+    }, {
+        "name": "Environment 2-1",
+        "link": "#env-2-1",
+    }, {
+        "name": "Environment 2-2",
+        "link": "#env-2-2",
+    }, {
+        "name": "Environment 2-3",
+        "link": "#env-2-3",
+    }, {
+        "name": "Environment 2-4",
+        "link": "#env-2-4",
+    }, {
+        "name": "Environment 2-5",
+        "link": "#env-2-5",
+    }],
+}, {
+    "name": "Region 3",
+    "icon": "flag-icon flag-icon-sg",
+    "sub": [{
+        "name": "Summary",
+        "link": "#reg-3",
+    }, {
+        "name": "Environment 3-1",
+        "link": "#env-3-1",
+    }, {
+        "name": "Environment 3-2",
+        "link": "#env-3-2",
+    }, {
+        "name": "Environment 3-3",
+        "link": "#env-3-3",
+    }, {
+        "name": "Environment 3-4",
+        "link": "#env-3-4",
+    }],
+}, ];
+
