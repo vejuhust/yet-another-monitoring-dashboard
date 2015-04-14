9b27ebf49972ef838b0a1021c7ef1f1b67596cd4
Wei Ye <vejuhust@gmail.com>
2015-04-14 23:32:46 +0800

    Add more mockup data for regions in content/menu-data.js

--- content/menu-data.js
+++ content/menu-data.js
@@ -59,6 +59,64 @@ var menu_env_data = [{
         "name": "Environment 3-4",
         "link": "env-3-4",
     }],
+}, {
+    "name": "Region 4",
+    "icon": "flag-icon flag-icon-za",
+    "sub": [{
+        "name": "Summary",
+        "link": "reg-4",
+    }, {
+        "name": "Environment 4-1",
+        "link": "env-4-1",
+    }, {
+        "name": "Environment 4-2",
+        "link": "env-4-2",
+    }],
+}, {
+    "name": "Region 5",
+    "icon": "flag-icon flag-icon-gb",
+    "sub": [{
+        "name": "Summary",
+        "link": "reg-5",
+    }, {
+        "name": "Environment 5-1",
+        "link": "env-5-1",
+    }, {
+        "name": "Environment 5-2",
+        "link": "env-5-2",
+    }, {
+        "name": "Environment 5-3",
+        "link": "env-5-3",
+    }, {
+        "name": "Environment 5-4",
+        "link": "env-5-4",
+    }, {
+        "name": "Environment 5-5",
+        "link": "env-5-5",
+    }],
+}, {
+    "name": "Region 6",
+    "icon": "flag-icon flag-icon-cn",
+    "sub": [{
+        "name": "Summary",
+        "link": "reg-6",
+    }, {
+        "name": "Environment 6-1",
+        "link": "env-6-1",
+    }, {
+        "name": "Environment 6-2",
+        "link": "env-6-2",
+    }],
+}, {
+    "name": "Region 7",
+    "icon": "flag-icon flag-icon-kr",
+    "sub": [{
+        "name": "Summary",
+        "link": "reg-7",
+    }, {
+        "name": "Environment 7-1",
+        "link": "env-7-1",
+    }],
 }, ];
 
 var menu_part_data = [{
