ae0e227db0aeb1988528f5c1e990c96571971bd8
Wei Ye <vejuhust@gmail.com>
2015-04-15 21:25:52 +0800

    Extract methods from content/page-render.js to new content/title-render.js

--- content/page-render.js
+++ content/page-render.js
@@ -8,39 +8,6 @@ function renderMainPage(tag_env, tag_part) {
   }
 }
 
-function updateContentTitleDesc (profile) {
-  var desc = $("<small/>", { id: "content-title-desc" });
-  if (profile.region_icon || profile.region_name) {
-    $("<i/>", {"class": profile.region_icon}).appendTo(desc);
-  }
-  if (profile.env_name) {
-    desc.append(" " + profile.env_name);
-  }
-  else if (profile.region_name) {
-    desc.append(" " + profile.region_name);
-  }
-  if (profile.part_name) {
-    desc.append(" - ");
-    $("<i/>", {"class": profile.part_icon}).appendTo(desc);
-    desc.append(" " + profile.part_name);
-  }
-  $("#content-title-desc").replaceWith(desc);
-  return $("#content-title-desc").html().length > 0;
-}
-
-function updateWebpageTitle (profile) {
-  var _title = "EGMD | ";
-  if (profile.env_name) {
-    _title += profile.env_name;
-  } else if (profile.region_name) {
-    _title += profile.region_name;
-  }
-  if (profile.part_name) {
-    _title += " - " + profile.part_name;
-  }
-  document.title = _title;
-}
-
 function restartStatusBoxIfHidden () {
   if ($("#status-box:hidden").length) {
     $("#status-subbox").empty();
