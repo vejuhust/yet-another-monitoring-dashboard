e920f7eb4fad58508c5c6adaa9538189f6cb4851
Wei Ye <vejuhust@gmail.com>
2015-04-15 10:06:30 +0800

    Append partner info in title desc of content/page-render.js

--- content/page-render.js
+++ content/page-render.js
@@ -3,9 +3,10 @@ console.log("hello page");
 function renderMainPage(tag_env, tag_part) {
   console.log("now " + tag_env + " " + tag_part);
 
-  var info_env = searchStaticDataByTagEnv(tag_env);
-  updateContentTitleDesc(info_env);
+  var info_env = searchConfigByTagEnv(tag_env);
+  var info_part = searchConfigByTagPart(tag_part);
 
+  updateContentTitleDesc($.extend(info_env, info_part));
 };
 
 function updateContentTitleDesc (info) {
@@ -15,13 +16,17 @@ function updateContentTitleDesc (info) {
     desc.append(" " + info.region_name + " ");
   }
   if (info.env_name) {
-    $("<i/>", {"class": info.env_name}).appendTo(desc);
     desc.append(" - " + info.env_name + " ");
+  }
+  if (info.part_name) {
+    desc.append(" - ");
+    $("<i/>", {"class": info.part_icon}).appendTo(desc);
+    desc.append(" " + info.part_name);
   }
   $("#content-title-desc").replaceWith(desc);
 }
 
-function searchStaticDataByTagEnv(tag_env) {
+function searchConfigByTagEnv(tag_env) {
   var _region_name, _region_icon, _region_link, _env_name, _env_link;
 
   for (var rl = menu_env_data.length, ri = 0; ri < rl; ri++) {
@@ -48,11 +53,31 @@ function searchStaticDataByTagEnv(tag_env) {
     }
   }
 
-  return result = {
+  return {
     "region_name": _region_name,
     "region_icon": _region_icon,
     "region_link": _region_link,
     "env_name": _env_name,
     "env_link": _env_link,
+  };
+}
+
+function searchConfigByTagPart (tag_part) {
+  var _part_name, _part_icon, _part_link;
+
+  for (var pl = menu_part_data.length, pi = 0; pi < pl; pi++) {
+    var partner = menu_part_data[pi];
+    if (partner.link == tag_part) {
+      _part_name = partner.name;
+      _part_icon = partner.icon;
+      _part_link = partner.link;
+      break;
+    }
+  }
+
+  return {
+    "part_name": _part_name,
+    "part_icon": _part_icon,
+    "part_link": _part_link,
   };
 }
