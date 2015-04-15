bf5d0bbf64cb2a9380608ea42e2391c920738b89
Wei Ye <vejuhust@gmail.com>
2015-04-15 16:51:30 +0800

    After status_box being hidden, new event will re-show it and clean it up

--- content/page-render.js
+++ content/page-render.js
@@ -4,7 +4,7 @@ function renderMainPage(tag_env, tag_part) {
   _profile = extractEnvPartProfile(tag_env, tag_part);
   if (updateContentTitleDesc(_profile)) {
     updateWebpageTitle(_profile);
-    addNewBoxInStatusBox(_profile);
+    addNewSubBoxInStatusBox(_profile);
   }
 }
 
@@ -39,12 +39,20 @@ function updateWebpageTitle (profile) {
   document.title = _title;
 }
 
-function addNewBoxInStatusBox (profile) {
+function restartStatusBoxIfHidden () {
+  if ($("#status-box:hidden").length) {
+    $("#status-subbox").empty();
+    $("#status-box").show();
+  }
+}
+
+function addNewSubBoxInStatusBox (profile) {
+  restartStatusBoxIfHidden();
   var _div = $("<div/>", { class: "alert alert-info alert-dismissable" });
   $('<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>').appendTo(_div);
   $('<h4><i class="icon fa fa-info"></i> </h4>').append("View Changed at " + $.format.date(new Date(), 'MM/dd/yyyy HH:mm:ss')).appendTo(_div);
   _div.append($("<div />").append($("#content-title-desc").html()));
-  $("#status-box").prepend(_div);
+  $("#status-subbox").prepend(_div);
 }
 
 function extractEnvPartProfile (tag_env, tag_part) {
