4119a5831ea28da31058b1d03e7f1f366def3443
Wei Ye <vejuhust@gmail.com>
2015-04-16 02:02:00 +0800

    Use top-right email icon as gauge data counter

--- content/fetch-data.js
+++ content/fetch-data.js
@@ -4,5 +4,8 @@ function fetchGauageRowData () {
     item['time'] = new Date();
     item['value'] *= 1 + (Math.random() - 0.5) * 0.1;
   });
-}
 
+  var _div = $("#gauge-count");
+  var _count = parseInt(_div.text()) + 1;
+  _div.text(_count);
+}
