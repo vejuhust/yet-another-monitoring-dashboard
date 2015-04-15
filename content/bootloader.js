59fd6e6d26ddb98957dcdcdb415efddad1080be3
Wei Ye <vejuhust@gmail.com>
2015-04-15 19:12:13 +0800

    Add simple code for time triggering of gauge

--- content/bootloader.js
+++ content/bootloader.js
@@ -9,3 +9,12 @@ window.onhashchange = _handlerRefreshMenuAndPage;
 
 // Update the menu and render the content
 _handlerRefreshMenuAndPage();
+
+(function myLoop (i) {          
+  setTimeout(function () {   
+    renderGaugeRowItems();
+    if (--i) {
+      myLoop(i);
+    }
+  }, 3000)
+})(1000);
