26b534ee3287378ca1be47ebbab1e1ec5a4d59cc
Wei Ye <vejuhust@gmail.com>
2015-04-20 17:37:30 +0800

    Replace left chart with hard-coded RPS chart

--- dist/js/pages/dashboard.js
+++ dist/js/pages/dashboard.js
@@ -176,23 +176,9 @@ $(function () {
     gridTextSize: 10
   });
 
-  //Donut Chart
-  var donut = new Morris.Donut({
-    element: 'sales-chart',
-    resize: true,
-    colors: ["#3c8dbc", "#f56954", "#00a65a"],
-    data: [
-      {label: "Download Sales", value: 12},
-      {label: "In-Store Sales", value: 30},
-      {label: "Mail-Order Sales", value: 20}
-    ],
-    hideHover: 'auto'
-  });
-
   //Fix for charts under tabs
   $('.box ul.nav a').on('shown.bs.tab', function (e) {
     area.redraw();
-    donut.redraw();
   });
 
 
